72cc7ac814506e475c449e0a95a51ac51098b2c2
marinejmartin <jmartin@marineinstruments.es>
2025-11-21 00:33:48 +0100

    Fix Docker issues: Node 20, db path, remove version

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Initialize database
-const db = new sqlite3.Database('./database.db');
+const db = new sqlite3.Database('./data/database.db');
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users (
