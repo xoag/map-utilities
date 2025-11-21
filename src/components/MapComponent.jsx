38066ad81b0ab9b16774006bc90bf91cf3fd124a
marinejmartin <jmartin@marineinstruments.es>
2025-11-21 01:18:44 +0100

    Add marker clustering: implement MarkerClusterGroup to group nearby markers for better performance and usability

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polygon, FeatureGroup, useMapEvents, useMap } from 'react-leaflet';
 import { EditControl } from 'react-leaflet-draw';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 import 'leaflet-draw/dist/leaflet.draw.css';
+import 'react-leaflet-markercluster/styles';
 import L from 'leaflet';
 
 // Fix default marker icon
@@ -374,18 +376,20 @@ function MapComponent({ token }) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        {markers.map((marker, index) => (
-          <Marker key={index} position={[marker.lat, marker.lng]}>
-            <Popup>
-              Marker {index + 1}
-              <br />
-              <button onClick={() => {
-                const newMarkers = markers.filter((_, i) => i !== index);
-                saveMarkers(newMarkers);
-              }}>Delete Marker</button>
-            </Popup>
-          </Marker>
-        ))}
+        <MarkerClusterGroup>
+          {markers.map((marker, index) => (
+            <Marker key={index} position={[marker.lat, marker.lng]}>
+              <Popup>
+                Marker {index + 1}
+                <br />
+                <button onClick={() => {
+                  const newMarkers = markers.filter((_, i) => i !== index);
+                  saveMarkers(newMarkers);
+                }}>Delete Marker</button>
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
         {polygons.map((poly, index) => (
           <div key={index}>
             <Polygon positions={poly.coords}>
